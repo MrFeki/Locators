79613bb19df5183c857d6fffed13b67bcc74e896
MrFeki <103600583+MrFeki@users.noreply.github.com>
2026-09-20 20:33:43 +0200

    Refactor API tests to RestSharp

--- Locators/Core/ApiClient.cs
+++ Locators/Core/ApiClient.cs
@@ -1,20 +1,58 @@
-using System.Net.Http;
-using System.Threading.Tasks;
+using RestSharp;
+using Serilog;
 
 namespace Locators.Core
 {
-    public class ApiClient
+    public class ApiClient : IDisposable
     {
-        private readonly HttpClient _httpClient;
+        private readonly RestClient _client;
 
-        public ApiClient(HttpClient httpClient)
+        public ApiClient(string baseUrl)
         {
-            _httpClient = httpClient;
+            if (!Uri.TryCreate(baseUrl, UriKind.Absolute, out var baseUri))
+                throw new ArgumentException("Base API URL must be a valid absolute URL.", nameof(baseUrl));
+
+            _client = new RestClient(new RestClientOptions(baseUri));
         }
 
-        public Task<HttpResponseMessage> SendAsync(HttpRequestMessage request)
+        public async Task<RestResponse> ExecuteAsync(RestRequest request)
         {
-            return _httpClient.SendAsync(request);
+            LogRequest(request);
+            var response = await _client.ExecuteAsync(request);
+            LogResponse(request, response);
+            return response;
+        }
+
+        public async Task<RestResponse<T>> ExecuteAsync<T>(RestRequest request)
+        {
+            LogRequest(request);
+            var response = await _client.ExecuteAsync<T>(request);
+            LogResponse(request, response);
+            return response;
+        }
+
+        private static void LogRequest(RestRequest request)
+        {
+            Log.Information("API request: {Method} {Resource}", request.Method, request.Resource);
+        }
+
+        private static void LogResponse(RestRequest request, RestResponse response)
+        {
+            if (response.IsSuccessful)
+            {
+                Log.Information("API response: {Method} {Resource} -> {StatusCode}",
+                    request.Method, request.Resource, (int)response.StatusCode);
+                return;
+            }
+
+            Log.Error("API response failed: {Method} {Resource} -> {StatusCode}. Error: {Error}. Body: {Body}",
+                request.Method, request.Resource, (int)response.StatusCode,
+                response.ErrorMessage, response.Content);
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
         }
     }
 }
